580e9559561cb2895cbd44b57fb8eaaca861b14a
Ajay19925 <ab10297@nyu.edu>
2022-11-01 12:41:36 +0530

    XSS Vulnerability part1

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
 # Create your tests here.
+class MyTestCase(TestCase):
+    def test_xss(self):
+        payload = "<script>alert('XSS Vulnerability Found')</script>"
+        params = {'director': payload}
+        response = self.client.get('/buy.html', params)
+        #self.assertEqual(response.context.get('director', None), payload)
+        print(response)
+        print(response.context)
+        self.assertEqual(response, payload)
