a57cc1c731853a87ff571b6fde556251eb62657b
Ajay19925 <ab10297@nyu.edu>
2022-11-01 19:24:18 +0530

    TestCases part1 - add

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -1,12 +1,27 @@
-from django.test import TestCase,Client
-
+from django.test import TestCase, Client
+from django.urls import reverse
+from LegacySite.models import *
+from LegacySite.views import *
+from django.http import HttpRequest
 # Create your tests here.
-class MyTestCase(TestCase):
-    def test_xss(self):
-        payload = "<script>alert('XSS Vulnerability Found')</script>"
-        params = {'director': payload}
-        response = self.client.get('/buy.html', params)
-        #self.assertEqual(response.context.get('director', None), payload)
-        print(response)
-        print(response.context)
-        self.assertEqual(response, payload)
+
+class TestResponse(TestCase):
+    def setup(self):
+        self.client= Client()
+
+    def test_xss_gift(self):
+        prouct1 = Product.objects.create(product_name = 'test', product_image_path= 'test', recommended_price = 1, description = 'test')
+        response = self.client.get('/gift' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
+        #print(response.content)
+        #self.assertnotContains(response, "XSS Vulnerability Found")
+        #self.assertTrue("XSS Vulnerability Found" in response.content)
+        #self.assertIn("XSS Vulnerability Found",response.content)
+        self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
+
+    def test_xss_buy(self):
+        prouct1 = Product.objects.create(product_name = 'test', product_image_path= 'test', recommended_price = 1, description = 'test')
+        response = self.client.get('/buy' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
+        #print(response.content)
+        #self.assertnotContains(response, "XSS Vulnerability Found")
+        #self.assertTrue("XSS Vulnerability Found" in response.content)
+        self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
