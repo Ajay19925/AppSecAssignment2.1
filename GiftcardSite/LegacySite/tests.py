5ab9be980ee09e984bd39298e3633993477bf027
Ajay19925 <ab10297@nyu.edu>
2022-11-06 11:10:45 +0530

    SQLinjection Testcase

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -3,29 +3,24 @@ from django.urls import reverse
 from LegacySite.models import *
 from LegacySite.views import *
 from django.http import HttpRequest
+import io
+
+
 # Create your tests here.
 
 class TestResponse(TestCase):
-    def setup(self):
-        self.client= Client()
+	def setup(self):
+		self.client= Client()
 
-    def test_xss_gift(self):
+	def test_xss_gift(self):
         prouct1 = Product.objects.create(product_name = 'test', product_image_path= 'test', recommended_price = 1, description = 'test')
-        response = self.client.get('/gift' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
-        #print(response.content)
-        #self.assertnotContains(response, "XSS Vulnerability Found")
-        #self.assertTrue("XSS Vulnerability Found" in response.content)
-        #self.assertIn("XSS Vulnerability Found",response.content)
-        self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
+		response = self.client.get('/gift' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
+		#print(response.content)
+		#self.assertnotContains(response, "XSS Vulnerability Found")
+		#self.assertTrue("XSS Vulnerability Found" in response.content)
+		#self.assertIn("XSS Vulnerability Found",response.content)
+		self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
 
-    def test_xss_buy(self):
-        prouct1 = Product.objects.create(product_name = 'test', product_image_path= 'test', recommended_price = 1, description = 'test')
-        response = self.client.get('/buy' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
-        #print(response.content)
-        #self.assertnotContains(response, "XSS Vulnerability Found")
-        #self.assertTrue("XSS Vulnerability Found" in response.content)
-        self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
-        
 	def test_xss_buy(self):
 		prouct1 = Product.objects.create(product_name = 'test', product_image_path= 'test', recommended_price = 1, description = 'test')
 		response = self.client.get('/buy' , {'director' : '<script>alert(''XSS Vulnerability Found'')</script>'})
@@ -33,6 +28,7 @@ class TestResponse(TestCase):
 		#self.assertnotContains(response, "XSS Vulnerability Found")
 		#self.assertTrue("XSS Vulnerability Found" in response.content)
 		self.assertContains(response, "&lt;script&gt;alert(XSS Vulnerability Found)&lt;/script&gt;", status_code=200)
+
 
 	def test_SQL_Injection(self):
 		self.client.post(reverse('Register'), {'uname': 'testuser1', 'pword': 'password1', 'pword2': 'password1'})
